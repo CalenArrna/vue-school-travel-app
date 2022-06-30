f1533b989353bf14f6c6aca185cdf252feb65c51
calenarrna <calenarrna@gmail.com>
2022-06-30 17:56:54 +0200

    Chapter 1 implemented

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+
+<div class="home">
+    <h1>All Destinations</h1>
+    <div class="destinations">
+    <router-link 
+    v-for="dest in destinations"
+    :key="dest.id"
+    :to="{name: 'destination.show', params:{id: dest.id}}"
+    >
+        <h2>{{dest.name}}</h2>
+        <img :src="`/images/${dest.image}`" :alt="dest.name">
+    </router-link>
+    </div>
+
+</div>
+
+</template>
+
+<script>
+import dataSource from '@/data.json';
+export default {
+    data() {
+        return {
+            destinations: dataSource.destinations
+        }
+    }
+}
+</script>
